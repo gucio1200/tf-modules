b9e418a2c66037f356b9af9a27439178eaf007ff
Gucio <kacper.gogol@gmail.com>
2026-04-14 20:41:59 +0200

    bump

--- federated_identity_credential/examples/simple/main.tf
+++ federated_identity_credential/examples/simple/main.tf
@@ -38,20 +38,20 @@ module "federated_identity_credential" {
   default_issuer                    = "https://oidc.prod-aks.azure.com/00000000-0000-0000-0000-000000000000/00000000-0000-0000-0000-000000000000/"
   default_user_assigned_identity_id = azurerm_user_assigned_identity.this.id
 
-  credentials = {
+  credentials = [
     # Using defaults (issuer and user_assigned_identity_id from module defaults)
-    minimal = {
+    {
       name      = "app-minimal"
       namespace = "default"
-    }
+    },
 
     # Overriding all possible values
-    custom = {
+    {
       name                      = "app-custom"
       namespace                 = "kube-system"
       issuer                    = "https://oidc.prod-aks.azure.com/11111111-1111-1111-1111-111111111111/11111111-1111-1111-1111-111111111111/"
       audience                  = "api://CustomAudience"
       user_assigned_identity_id = azurerm_user_assigned_identity.this.id
     }
-  }
+  ]
 }

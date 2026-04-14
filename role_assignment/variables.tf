8deb950fdba6335b45aec54ea79d9a3d7362aa97
Gucio <kacper.gogol@gmail.com>
2026-04-14 16:24:46 +0200

    bump

--- role_assignment/variables.tf
+++ role_assignment/variables.tf
@@ -1,18 +1,11 @@
 variable "assignments" {
   description = "A map of objects, each describing an azurerm_role_assignment resource. The map key should be a stable, plan-time known string (like the role name)."
-  type = map(object({
-    principal_id                     = optional(any)
-    scope                            = any
-    role_definition_name             = optional(any)
-    role_definition_id               = optional(any)
-    description                      = optional(string, null)
-    skip_service_principal_aad_check = optional(bool, false)
-  }))
+  type        = any
 
   validation {
     condition = length([
       for v in var.assignments :
-      v if(v.role_definition_name != null ? 1 : 0) + (v.role_definition_id != null ? 1 : 0) != 1
+      v if(try(v.role_definition_name, null) != null ? 1 : 0) + (try(v.role_definition_id, null) != null ? 1 : 0) != 1
     ]) == 0
     error_message = "Each assignment must provide either the 'role_definition_name' or 'role_definition_id' property, but not both. The value can be a single string or a list of strings."
   }
@@ -20,7 +13,7 @@ variable "assignments" {
   validation {
     condition = length([
       for v in var.assignments :
-      v if v.principal_id == null && var.default_principal_id == null
+      v if try(v.principal_id, null) == null && var.default_principal_id == null
     ]) == 0
     error_message = "Each assignment must provide a 'principal_id' (can be a single string or a list of strings) or a 'default_principal_id' must be set for the module."
   }
@@ -28,7 +21,7 @@ variable "assignments" {
   validation {
     condition = length([
       for v in var.assignments :
-      v if v.scope == null || length(try(tolist(v.scope), [v.scope])) == 0
+      v if try(v.scope, null) == null || length(try(tolist(v.scope), [v.scope])) == 0
     ]) == 0
     error_message = "Each assignment must provide a valid 'scope' (can be a single string or a list of strings)."
   }
